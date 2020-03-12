9c50735f4022ec2e45f87391d7bb829c48f35690
Matt Carey <matt@authorityspoke.com>
2020-03-11 21:54:50 -0500

    add test for creating Enactment

---
+++ tests/test_enactments.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+from legislice.enactments import Enactment
+
+
+class TestMakeEnactment:
+    def test_enactment_without_nesting(self):
+        s1 = Enactment(
+            node="/test/acts/47/1",
+            start_date=date(1935, 4, 1),
+            heading="Short title",
+            content=(
+                "This Act may be cited as the Australian Beard Tax"
+                " (Promotion of Enlightenment Values) Act 1934."
+            ),
+        )
+        assert s1.end_date is None
