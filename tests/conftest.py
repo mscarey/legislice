de5dd40ccc5f94a547e657ed54fd9ec9801c0563
Matt Carey <matt@authorityspoke.com>
2020-08-12 03:50:27 -0500

    remove condition on loading requirements in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,14 @@ from anchorpoint import TextQuoteSelector
 import pytest
 
 
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        # Replace the Authorization request header with "DUMMY" in cassettes
+        "filter_headers": [("authorization", "DUMMY")],
+    }
+
+
 @pytest.fixture(scope="class")
 def section6d():
     return {
