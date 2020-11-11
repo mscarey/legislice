58428820c988f79785824adbd1830c9877ae3b11
Matt Carey <matt@authorityspoke.com>
2020-11-11 05:32:26 -0600

    exclude unknown in TextVersionSchema

--- legislice/schemas.py
+++ legislice/schemas.py
@@ -122,6 +122,9 @@ class TextVersionSchema(Schema):
     url = fields.Str(required=False)
     id = fields.Int(required=False)
 
+    class Meta:
+        unknown = EXCLUDE
+
     @post_load
     def make_object(self, data, **kwargs):
         return self.__model__(**data)
